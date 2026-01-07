a62d30efeb085915c4edcdc7fedccc338d8862dc
Diogo <fmlsp2015@gmail.com>
2026-01-07 15:38:31 -0300

    feat: att width and height on resize

--- main.go
+++ main.go
@@ -26,7 +26,11 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
+  
+	case tea.WindowSizeMsg:
+	m.width = msg.Width
+	m.height = msg.Height
+	
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q", "ctrl+c":
